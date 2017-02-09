cfa19dca5a460cb72da1adad0ae55665c430f1e4
u6k <u6k.apps@gmail.com>
2017-02-09 14:14:11 +0900

    例外時に適切なステータスコードを返すようにしました。

---
+++ src/main/java/me/u6k/bookmark_bundler/ExceptionHandler.java
@@ -0,0 +1,32 @@
+
+package me.u6k.bookmark_bundler;
+
+import me.u6k.bookmark_bundler.exception.BookmarkDuplicateException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @org.springframework.web.bind.annotation.ExceptionHandler({ IllegalArgumentException.class, BookmarkDuplicateException.class })
+    @ResponseBody
+    public ExceptionVO badRequestHandler(Exception e) {
+        ExceptionVO exceptionVO = new ExceptionVO();
+        exceptionVO.exception = e.getClass().getName();
+        exceptionVO.message = e.getMessage();
+
+        return exceptionVO;
+    }
+
+    class ExceptionVO {
+
+        public String exception;
+
+        public String message;
+
+    }
+
+}
